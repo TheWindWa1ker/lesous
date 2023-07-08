7f17643f7ca9cf4d81b163d3c024f6fc6b4871a4
Mariko Rempel <96768695+TheWindWa1ker@users.noreply.github.com>
2023-07-08 18:50:42 +0800

    Update Offsets.cpp
    update Steam# v3.0.36.26

--- Offsets.cpp
+++ Offsets.cpp
@@ -5,16 +5,16 @@ namespace offsets
 {
     // buttons
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    const long IN_ATTACK = 0x074034b0;            // [Buttons] -> in_attack
+    const long IN_ATTACK = 0x0743d330;            // [Buttons] -> in_attack
     // core
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const long REGION = 0x140000000;              // [Mine]          -> Region
-    const long LEVEL = 0x165bd30;                 // [Miscellaneous] -> LevelName
-    const long LOCAL_PLAYER = (0x02139480 + 0x8);       // [Miscellaneous] -> AVC_GameMovement+0x8
-    const long ENTITY_LIST = 0x1d87e88;           // [Miscellaneous] -> cl_entitylist
+    const long LEVEL = 0x16fa230;                 // [Miscellaneous] -> LevelName
+    const long LOCAL_PLAYER = (0x022036C0 + 0x8);       // [Miscellaneous] -> AVC_GameMovement+0x8
+    const long ENTITY_LIST = 0x1e53c68;           // [Miscellaneous] -> cl_entitylist
     // entity
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    const long LOCAL_ORIGIN = 0x0054; // [DataMap.CBaseViewModel]    -> m_localOrigin
+    const long LOCAL_ORIGIN = 0x0158; // [DataMap.CBaseViewModel]    -> m_localOrigin
     // player
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const long GLOW_ENABLE = (0x03c0 + 0x8);        // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x8
@@ -23,10 +23,10 @@ namespace offsets
     const long TEAM_NUMBER = 0x044c;                // [RecvTable.DT_BaseEntity]        -> m_iTeamNum
     const long NAME = 0x0589;                       // [RecvTable.DT_BaseEntity]        -> m_iName
     const long LIFE_STATE = 0x0798;                 // [RecvTable.DT_Player]            -> m_lifeState
-    const long VEC_PUNCH_WEAPON_ANGLE = 0x24c0;     // [DataMap.C_Player]               -> m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
-    const long VIEW_ANGLE = (0x25bc - 0x14);        // [DataMap.C_Player]               -> m_ammoPoolCapacity - 0x14
+    const long VEC_PUNCH_WEAPON_ANGLE = 0x24b8;     // [DataMap.C_Player]               -> m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
+    const long VIEW_ANGLE = (0x25b4 - 0x14);        // [DataMap.C_Player]               -> m_ammoPoolCapacity - 0x14
     const long BLEEDOUT_STATE = 0x2750;             // [RecvTable.DT_Player]            -> m_bleedoutState
-    const long ZOOMING = 0x1c61;                    // [RecvTable.DT_Player]            -> m_bZooming
-    const long LAST_VISIBLE_TIME = 0x1A78;          // [Miscellaneous]                  -> CPlayer!lastVisibleTime
+    const long ZOOMING = 0x1c51;                    // [RecvTable.DT_Player]            -> m_bZooming
+    const long LAST_VISIBLE_TIME = 0x1A70;          // [Miscellaneous]                  -> CPlayer!lastVisibleTime
     const long CURRENT_SHIELDS = 0x0170;             // m_shieldHealth
 }
